b7291274d6fa197458f8581dbfbd2f124c8df3cb
Matt <matt@primestudiosllc.com>
2012-04-18 02:42:43 -0700

    readme

--- app/config/config.php
+++ app/config/config.php
@@ -218,7 +218,7 @@ $config['cache_path'] = '';
 | enabled you MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = "";
+$config['encryption_key'] = "5FWJB3S6ETEahxiERNZuwl8SeeqftfKtkPKl";
 
 /*
 |--------------------------------------------------------------------------
